6bc6ece0db0835e0f975352716ae11d690ad5f56
JoshAtticus <72828296+JoshAtticus@users.noreply.github.com>
2024-12-01 10:48:21 +0800

    ae

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -8,10 +8,10 @@ export const linkUrl =
 	localStorage.getItem("meower_linkurl") ||
 	(beta
 		? "wss://beta.meower.org/api/v0/cloudlink"
-		: "wss://api.meower.org/v0/cloudlink");
+		: "wss://meower-cl.joshatticus.site/v0/cloudlink");
 export const apiUrl =
 	localStorage.getItem("meower_apiurl") ||
-	(beta ? "https://beta.meower.org/api/" : "https://api.meower.org/");
+	(beta ? "https://beta.meower.org/api/" : "https://meower-api.joshatticus.site/");
 export const uploadsUrl = localStorage.getItem("meower_uploadsurl") || "https://uploads.meower.org/";
 export const encodeApiURLParams =
 	localStorage.getItem("meower_encodeapi") || false;
